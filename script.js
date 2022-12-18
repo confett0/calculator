67e7e4f4d1b1cb4c28f4a8e9d62ee4fa17884e01
confett0 <118047961+confett0@users.noreply.github.com>
2022-12-18 17:56:10 +0100

    Add basic calculator functions

---
+++ script.js
@@ -0,0 +1,21 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const operate = (num1, num2, operator) => {
+    switch (operator) {
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+    }
+}
