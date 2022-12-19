18a72647f7e631ce13aeeeb544edcc54f16f7d4a
confett0 <118047961+confett0@users.noreply.github.com>
2022-12-19 13:48:41 +0100

    Add event listener for numbers

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const operators = document.querySelectorAll('.operator');
+const operands = document.querySelectorAll('.number');
+const result = document.querySelector('.result');
+const display = document.querySelector('.display');
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
@@ -19,3 +24,9 @@ const operate = (num1, num2, operator) => {
             break;
     }
 }
+
+operands.forEach(operand => operand.addEventListener('click',function() {
+    let currentNum = operand.value;
+    display.innerHTML = currentNum;
+}))
+
